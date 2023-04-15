c9ed1d90654f1832c0cde199af034cc363d280ca
Evelina Norlin <evelina.norlin@hotmail.com>
2023-04-15 19:15:17 +0200

    added burger in desktop

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ import { renderNav } from "./modules/nav";
 
 renderPortfolio();
 renderNav();
+
+console.log(window)
+
+//window.onscroll(console.log(window.scrollY))
--- modules/nav.js
+++ modules/nav.js
@@ -1,9 +1,25 @@
 export function renderNav(){
   let burgerBtn = document.getElementById('burger');
-  let menu = document.getElementById('nav-menu')
+  let menu = document.getElementById('nav-menu');
+  let burgerDesc = document.getElementById('burger-desc');
+  let desktopNav = document.getElementById('desktop-nav');
+  let burgerMenu = document.getElementById('top-bar');
 
   burgerBtn.addEventListener('click', () => {
-    burgerBtn.classList.toggle('open')
-    menu.classList.toggle('closed-menu')
+    burgerBtn.classList.toggle('open');
+    menu.classList.toggle('closed-menu');
   })
+
+  burgerDesc.addEventListener('click', () => {
+    burgerDesc.classList.toggle('open');
+  })
+
+  window.addEventListener('scroll', () => {
+    console.log(scrollY)
+    if(window.scrollY > 100){
+      burgerDesc.style.display = 'block';
+    } else{
+      burgerDesc.style.display = 'none';
+    }
+  });
 }
